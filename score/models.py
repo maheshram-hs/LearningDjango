f95b9772a09ed7122acfade02b506708fab93a36
Maheshram <smaheshram@yahoo.com>
2023-12-16 14:19:16 +0000

    Learning django models

--- score/models.py
+++ score/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Score(models.Model):
+    name = models.CharField(max_length=50)
+    value = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.name
